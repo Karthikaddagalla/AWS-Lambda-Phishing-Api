6b87ea9b4458f03301dc63737ece2d64edbf2f50
Karthik Satya Sai Addagalla <addagallakarthik@gmail.com>
2024-02-20 18:17:41 +0530

    Updated handler.py for awakening the server

--- handler.py
+++ handler.py
@@ -14,18 +14,23 @@ from predictor import predictor
 def hello(event, context):
     url = event['queryStringParameters']['url']
 
-    result = predictor(url)
+    if url=="up":    # Waking the system up when page is reloaded to avoid delays
+        res_body={"message": "Thanks for waking me up"}
 
-    res_body = {
-        "message": "Given link is Good link, if below output is 0",
-        "output": result
-    }
+    else:
+        result = predictor(url)
+
+        res_body = {
+            "message": "Given link is Good link, if below output is 0",
+            "output": result
+        }
 
 
     http_res = {}
     http_res['statusCode'] = 200
-    http_res['headers'] = {}
-    http_res['headers']['Content-Type'] = 'application/json'
+    http_res['headers'] = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
     http_res['body'] = json.dumps(res_body)
     
     return http_res
@@ -43,59 +48,4 @@ def hello(event, context):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# try:
-#     import unzip_requirements
-# except ImportError:
-#     pass
-
-# import json
-# import os
-
-# def hello(event, context):
-#     # Print current directory
-   
-
-#     # Access the contents of gradient_classifier_98
-#     file_path = 'Models/gradient_classifier_98'
-#     try:
-#         with open(file_path, 'r') as file:
-#             classifier_contents = file.read()
-#     except FileNotFoundError:
-#         classifier_contents = "File not found."
-
-#     # Construct response body
-#     res_body = {
-#         "message": "Given link is Good link, if below output is 0",
-#         "output_total": os.listdir('.'),
-#         "output_models": os.listdir('Models'),
-#         "gradient_classifier_98_contents": classifier_contents
-#     }
-
-#     # Construct HTTP response
-#     http_res = {
-#         "statusCode": 200,
-#         "headers": {
-#             "Content-Type": "application/json"
-#         },
-#         "body": json.dumps(res_body)
-#     }
-    
-#     return http_res
+_res
